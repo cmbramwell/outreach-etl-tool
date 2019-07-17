2d611203431fd6360b96c7fbc8a72bfbd7b6c29f
cmbramwell <bramwell.christian@gmail.com>
2019-07-17 16:01:12 -0500

    add gmail script to email log file once etl script is complete

---
+++ gmail.py
@@ -0,0 +1,107 @@
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+from apiclient import discovery
+from apiclient import errors
+from httplib2 import Http
+
+import base64
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import mimetypes
+import os
+
+from apiclient import errors
+
+# Authentication Flow
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+path_cred = '/Users/christianbramwell/Documents/Turn:River Capital/Coding Scripts/google-cloud-credentials/'
+
+creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+if os.path.exists(path_cred + 'token.pickle'):
+    with open(path_cred + 'token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            path_cred + 'credentials.json', SCOPES)
+        creds = flow.run_local_server()
+    # Save the credentials for the next run
+    with open(path_cred + 'token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+GMAIL = discovery.build('gmail', 'v1', credentials=creds, cache_discovery=False)
+
+def SendMessageWithAttachment(sender, to, subject, message_text, file_dir,
+                                filename):
+    """Create a message for an email.
+
+    Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+    file_dir: The directory containing the file to be attached.
+    filename: The name of the file to be attached.
+
+    Returns:
+    An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    msg = MIMEText(message_text)
+    message.attach(msg)
+
+    path = os.path.join(file_dir, filename)
+    content_type, encoding = mimetypes.guess_type(path)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(path, 'rb')
+        msg = MIMEText(fp.read(), _subtype=sub_type, _charset='utf-8')
+        fp.close()
+    elif main_type == 'image':
+        fp = open(path, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type, _charset='utf-8')
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(path, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type, _charset='utf-8')
+        fp.close()
+    else:
+        fp = open(path, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+    b64_bytes = base64.urlsafe_b64encode(message.as_bytes())
+    b64_string = b64_bytes.decode()
+    body = {'raw': b64_string}
+
+    GMAIL.users().messages().send(userId='me', body=body).execute()
+
+    return {'raw': body}
+
+SendMessageWithAttachment('christian@turnriver.com', 'christian@turnriver.com', 'test', 'test', os.getcwd(), 'log_outreach_script.log')
